8ea858ff70b46a6d13aba7cdd3e457ceb3125171
wmartinezm <wlsn.martinez@gmail.com>
2023-10-18 10:33:30 -0600

    Cleaning... house keeping

--- test/test_schedule/schedule.c
+++ test/test_schedule/schedule.c
@@ -47,11 +47,7 @@ void test_priority_inversion(void)
                  (k_thread_entry_t)priority_inversion, 
                  NULL, &sem, NULL, K_PRIO_PREEMPT(3), K_MSEC(12), &high_stats,
                  &elapsed_stats);
-
-    // run_analyzer((k_thread_entry_t)priority_inversion, &sem,
-    //              K_PRIO_PREEMPT(4), K_MSEC(10), &low_stats,
-    //              K_PRIO_PREEMPT(3), K_MSEC(12), &high_stats,
-    //              &elapsed_stats);
+                 
     TEST_ASSERT_UINT64_WITHIN(1000, 0, high_stats);
     TEST_ASSERT_UINT64_WITHIN(100000, 5000000, low_stats);
 }
@@ -65,11 +61,7 @@ void test_coop_same_priority_busy(void)
                  (k_thread_entry_t)busy_busy, 
                  NULL, NULL, NULL, K_PRIO_COOP(3), K_MSEC(12), &second_stats,
                  &elapsed_stats);
-
-    // run_analyzer((k_thread_entry_t)busy_busy,  NULL,
-    //              K_PRIO_COOP(3), K_MSEC(10), &first_stats,
-    //              K_PRIO_COOP(3), K_MSEC(12), &second_stats,
-    //              &elapsed_stats);
+                 
     TEST_ASSERT_EQUAL_UINT64(0, second_stats);
     TEST_ASSERT_UINT64_WITHIN(100000, 5000000, first_stats);
 }
@@ -83,11 +75,7 @@ void test_coop_same_priority_yield(void)
                  (k_thread_entry_t)busy_yield, 
                  NULL, NULL, NULL, K_PRIO_COOP(3), K_MSEC(10), &second_stats,
                  &elapsed_stats);
-
-    // run_analyzer((k_thread_entry_t)busy_yield,  NULL,
-    //              K_PRIO_COOP(3), K_MSEC(10), &first_stats,
-    //              K_PRIO_COOP(3), K_MSEC(10), &second_stats,
-    //              &elapsed_stats);
+                 
     TEST_ASSERT_UINT64_WITHIN(100000, 2500000, first_stats);
     TEST_ASSERT_UINT64_WITHIN(100000, 2500000, second_stats);
 }
@@ -101,10 +89,6 @@ void test_preempt_same_priority_busy(void)
                  NULL, NULL, NULL, K_PRIO_PREEMPT(3), K_MSEC(12), &second_stats,
                  &elapsed_stats);
 
-    // run_analyzer((k_thread_entry_t)busy_busy,  NULL,
-    //              K_PRIO_PREEMPT(3), K_MSEC(10), &first_stats,
-    //              K_PRIO_PREEMPT(3), K_MSEC(12), &second_stats,
-    //              &elapsed_stats);
     TEST_ASSERT_EQUAL_UINT64(0, second_stats);
     TEST_ASSERT_UINT64_WITHIN(100000, 5000000, first_stats);
 }
@@ -118,10 +102,7 @@ void test_preempt_same_priority_yield(void)
                  (k_thread_entry_t)busy_yield, 
                  NULL, NULL, NULL, K_PRIO_PREEMPT(3), K_MSEC(10), &second_stats,
                  &elapsed_stats);
-    // run_analyzer((k_thread_entry_t)busy_yield,  NULL,
-    //              K_PRIO_PREEMPT(3), K_MSEC(10), &first_stats,
-    //              K_PRIO_PREEMPT(3), K_MSEC(10), &second_stats,
-    //              &elapsed_stats);
+                 
     TEST_ASSERT_UINT64_WITHIN(100000, 2500000, first_stats);
     TEST_ASSERT_UINT64_WITHIN(100000, 2500000, second_stats);
 }
@@ -134,10 +115,7 @@ void test_coop_priority_high_first_busy(void)
                  (k_thread_entry_t)busy_busy, 
                  NULL, NULL, NULL, K_PRIO_COOP(3), K_MSEC(12), &low_stats,
                  &elapsed_stats);
-    // run_analyzer((k_thread_entry_t)busy_busy,  NULL,
-    //              K_PRIO_COOP(3), K_MSEC(12), &low_stats,
-    //              K_PRIO_COOP(2), K_MSEC(10), &high_stats,
-    //              &elapsed_stats);
+                 
     TEST_ASSERT_EQUAL_UINT64(0, low_stats);
     TEST_ASSERT_UINT64_WITHIN(100000, 5000000, high_stats);
 }
@@ -152,10 +130,6 @@ void test_coop_priority_low_first_busy(void)
                  NULL, NULL, NULL, K_PRIO_COOP(2), K_MSEC(12), &high_stats,
                  &elapsed_stats);
 
-    // run_analyzer((k_thread_entry_t)busy_busy,  NULL,
-    //              K_PRIO_COOP(3), K_MSEC(10), &low_stats,
-    //              K_PRIO_COOP(2), K_MSEC(12), &high_stats,
-    //              &elapsed_stats);
     TEST_ASSERT_EQUAL_UINT64(0, high_stats);
     TEST_ASSERT_UINT64_WITHIN(100000, 5000000, low_stats);
 }
@@ -169,10 +143,6 @@ void test_coop_priority_high_first_yield(void)
                  NULL, NULL, NULL, K_PRIO_COOP(3), K_MSEC(10), &low_stats,
                  &elapsed_stats);
 
-    // run_analyzer((k_thread_entry_t)busy_yield,  NULL,
-    //              K_PRIO_COOP(3), K_MSEC(10), &low_stats,
-    //              K_PRIO_COOP(2), K_MSEC(12), &high_stats,
-    //              &elapsed_stats);
     TEST_ASSERT_UINT64_WITHIN(5000, 0, high_stats);
     TEST_ASSERT_UINT64_WITHIN(100000, 5000000, low_stats);
 }
@@ -186,10 +156,6 @@ void test_coop_priority_low_first_yield(void)
                  NULL, NULL, NULL, K_PRIO_COOP(2), K_MSEC(12), &high_stats,
                  &elapsed_stats);
                  
-    // run_analyzer((k_thread_entry_t)busy_yield,  NULL,
-    //              K_PRIO_COOP(3), K_MSEC(10), &low_stats,
-    //              K_PRIO_COOP(2), K_MSEC(12), &high_stats,
-    //              &elapsed_stats);
     TEST_ASSERT_UINT64_WITHIN(5000, 0, low_stats);
     TEST_ASSERT_UINT64_WITHIN(100000, 5000000, high_stats);
 }
@@ -206,10 +172,6 @@ void test_preempt_priority_high_first_busy(void)
                  NULL, NULL, NULL, K_PRIO_PREEMPT(3), K_MSEC(10), &low_stats,
                  &elapsed_stats);
 
-    // run_analyzer((k_thread_entry_t)busy_busy,  NULL,
-    //              K_PRIO_PREEMPT(3), K_MSEC(10), &low_stats,
-    //              K_PRIO_PREEMPT(2), K_MSEC(10), &high_stats,
-    //              &elapsed_stats);
     TEST_ASSERT_UINT64_WITHIN(5000, 0, low_stats);
     TEST_ASSERT_UINT64_WITHIN(100000, 5000000, high_stats);
 }
@@ -224,10 +186,6 @@ void test_preempt_priority_low_first_busy(void)
                  NULL, NULL, NULL, K_PRIO_PREEMPT(2), K_MSEC(10), &high_stats,
                  &elapsed_stats);
 
-    // run_analyzer((k_thread_entry_t)busy_busy,  NULL,
-    //              K_PRIO_PREEMPT(3), K_MSEC(10), &low_stats,
-    //              K_PRIO_PREEMPT(2), K_MSEC(10), &high_stats,
-    //              &elapsed_stats);
     TEST_ASSERT_UINT64_WITHIN(5000, 0, low_stats);
     TEST_ASSERT_UINT64_WITHIN(100000, 5000000, high_stats);
 }
@@ -242,10 +200,6 @@ void test_preempt_priority_high_first_yield(void)
                  NULL, NULL, NULL, K_PRIO_PREEMPT(3), K_MSEC(10), &low_stats,
                  &elapsed_stats);
 
-    // run_analyzer((k_thread_entry_t)busy_yield,  NULL,
-    //              K_PRIO_PREEMPT(3), K_MSEC(10), &low_stats,
-    //              K_PRIO_PREEMPT(2), K_MSEC(10), &high_stats,
-    //              &elapsed_stats);
     TEST_ASSERT_UINT64_WITHIN(5000, 0, low_stats);
     TEST_ASSERT_UINT64_WITHIN(100000, 5000000, high_stats);
 }
@@ -260,24 +214,9 @@ void test_preempt_priority_low_first_yield(void)
                  NULL, NULL, NULL, K_PRIO_PREEMPT(2), K_MSEC(10), &high_stats,
                  &elapsed_stats);
 
-    // run_analyzer((k_thread_entry_t)busy_yield,  NULL,
-    //              K_PRIO_PREEMPT(3), K_MSEC(10), &low_stats,
-    //              K_PRIO_PREEMPT(2), K_MSEC(10), &high_stats,
-    //              &elapsed_stats);
     TEST_ASSERT_UINT64_WITHIN(5000, 0, low_stats);
     TEST_ASSERT_UINT64_WITHIN(100000, 5000000, high_stats);
 }
-
-// void test_mix__priority__yield(void)
-// {
-//     uint64_t low_stats, high_stats, elapsed_stats;
-//     run_analyzer((k_thread_entry_t)busy_yield,  NULL,
-//                  K_PRIO_PREEMPT(3), K_MSEC(10), &low_stats,
-//                  K_PRIO_COOP(3), K_MSEC(12), &high_stats,
-//                  &elapsed_stats);
-//     TEST_ASSERT_UINT64_WITHIN(5000, 0, low_stats);
-//     TEST_ASSERT_UINT64_WITHIN(100000, 5000000, high_stats);
-// }
 
 // Write tests for two threads: one should run busy_sleep and the other busy_yield 
 void test_preempt_priority_mix(void)
@@ -290,12 +229,6 @@ void test_preempt_priority_mix(void)
                  NULL, NULL, NULL, K_PRIO_PREEMPT(2), K_MSEC(10), &high_stats,
                  &elapsed_stats);
 
-    // run_analyzer_split(5000,
-    //                    (k_thread_entry_t)busy_busy, NULL, NULL, NULL,
-    //                    K_PRIO_PREEMPT(3), K_MSEC(10), &low_stats,
-    //                    (k_thread_entry_t)busy_sleep, NULL, NULL, NULL,
-    //                    K_PRIO_PREEMPT(2), K_MSEC(10), &high_stats,
-    //                    &elapsed_stats);
     TEST_ASSERT_UINT64_WITHIN(1000, 10000, high_stats);
     TEST_ASSERT_UINT64_WITHIN(100000, 5000000, low_stats);
 }
@@ -324,6 +257,6 @@ int main (void)
     RUN_TEST(test_preempt_priority_low_first_yield);
 
     RUN_TEST(test_preempt_priority_mix);
-    
+
     return UNITY_END();
 }
